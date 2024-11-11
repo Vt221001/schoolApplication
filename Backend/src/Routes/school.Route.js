2fdd9d71b08ac0d202819e8aaeaa699b76370679
vedansh tiwari <vt2855028@gmail.com>
2024-11-12 02:15:43 +0530

    Initial commit

--- Backend/src/Routes/school.Route.js
+++ Backend/src/Routes/school.Route.js
@@ -1,13 +1,20 @@
-import express from 'express';
-import { createSchool, deleteSchool, getSchool, getSchools, updateSchool } from '../Controller/school.Controller.js';
+import express from "express";
+import {
+    createSchool,
+    deleteSchool,
+    getSchool,
+    getSchoolNameByCode,
+    getSchools,
+    updateSchool,
+} from "../Controller/school.Controller.js";
 
 const router = express.Router();
 
-router.post('/create-school/', createSchool);
-router.get('/get-schools', getSchools);
-router.get('/get-school/:id', getSchool);
-router.put('/update-school/:id',updateSchool);
-router.delete('/delete-school/:id', deleteSchool);
+router.post("/create-school/", createSchool);
+router.get("/get-schools", getSchools);
+router.get("/get-school/:id", getSchool);
+router.put("/update-school/:id", updateSchool);
+router.delete("/delete-school/:id", deleteSchool);
+router.post("/school-name-byschoolcode", getSchoolNameByCode);
 
-
-export {router as schoolRoute};
+export { router as schoolRoute };
