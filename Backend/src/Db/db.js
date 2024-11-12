068a2778efa796e2200aac947b89dd4f8bb31d5e
vedansh tiwari <vt2855028@gmail.com>
2024-11-12 14:00:26 +0530

    Initial commit

--- Backend/src/Db/db.js
+++ Backend/src/Db/db.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constant.js";
+// import { DB_NAME } from "../constant.js";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
-        console.log(`\n MongoDB Connected...!!`);
+        await mongoose.connect(
+            `${process.env.MONGO_URI}/${process.env.DB_NAME}`
+        );
+        console.log(`\n MongoDB Connected...!!${process.env.DB_NAME}`);
     } catch (err) {
         console.log("MongoDB connection error", err);
         process.exit(1);
