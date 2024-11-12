5f2e12fc21de26a21b4f0fe88a9df34c1d166847
Hemant Medhsia <hemantmedhsia@gmail.com>
2024-11-12 13:28:10 +0530

    initial commit

--- Backend/src/Db/db.js
+++ Backend/src/Db/db.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
-// import { DB_NAME } from "../constant.js";
+import { DB_NAME } from "../constant.js";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(`${process.env.MONGO_URI}/${process.env.DB_NAME}`);
+        await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
         console.log(`\n MongoDB Connected...!!`);
     } catch (err) {
         console.log("MongoDB connection error", err);
