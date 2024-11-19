2109f9fcb3dcf146d5f45e9964dc8107b4b6111f
vedansh tiwari <vt2855028@gmail.com>
2024-11-19 16:41:01 +0530

    Initial commit

--- Backend/src/Utils/generateAcessToken.js
+++ Backend/src/Utils/generateAcessToken.js
@@ -3,6 +3,6 @@ export const generateAccessToken = (user) => {
     return jwt.sign(
       { id: user._id, role: user.role },
       process.env.ACCESS_TOKEN_SECRET,
-      { expiresIn: "15m" }
+      { expiresIn: "7d" }
     );
 };
