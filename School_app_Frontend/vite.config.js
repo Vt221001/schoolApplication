292b44133fa06163d1ce4bf5f7c4651058be71f1
vedansh tiwari <vt2855028@gmail.com>
2024-12-28 18:41:59 +0530

    Initial commit

--- School_app_Frontend/vite.config.js
+++ School_app_Frontend/vite.config.js
@@ -1,11 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   server: {
+//     host: '0.0.0.0',
+//     port: 5174,
+//   },
+//   plugins: [react()],
+// })
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
 export default defineConfig({
-  server: {
-    host: '0.0.0.0',
-    port: 5174,
-  },
   plugins: [react()],
-})
+});
