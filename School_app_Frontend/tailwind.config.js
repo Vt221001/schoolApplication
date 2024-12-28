374806ae5e0c109750b2ff410c121df571079b92
vedansh tiwari <vt2855028@gmail.com>
2024-12-28 18:06:27 +0530

    Initial commit

--- School_app_Frontend/tailwind.config.js
+++ School_app_Frontend/tailwind.config.js
@@ -1,29 +1,29 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        'heading': ['Poppins', 'sans-serif'],
-        'body': ['Open Sans', 'sans-serif'],
+        heading: ["Poppins", "sans-serif"],
+        body: ["Open Sans", "sans-serif"],
       },
       colors: {
-        customBackground: "#283046", // Custom background color
-        customText: "#FFFFFF", // Custom text color
-        placeholder: "#A0AEC0", // Placeholder text color
+        customBackground: "#283046",
+        customText: "#FFFFFF",
+        placeholder: "#A0AEC0",
         gold: {
-          500: '#d4af37',
-          600: '#b7950b',
-          700: '#a17c00',
+          500: "#d4af37",
+          600: "#b7950b",
+          700: "#a17c00",
         },
-        'vibrant-blue': '#007BFF',
-        'vibrant-green': '#28A745',
-        'vibrant-orange': '#FFC107',
-        'vibrant-red': '#DC3545',
+        "vibrant-blue": "#007BFF",
+        "vibrant-green": "#28A745",
+        "vibrant-orange": "#FFC107",
+        "vibrant-red": "#DC3545",
       },
       container: {
         center: true,
-        padding: "2rem", // Center the container with padding
+        padding: "2rem",
       },
       transitionProperty: {
         width: "width",
